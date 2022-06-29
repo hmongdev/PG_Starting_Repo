edae03e9c3dcc8fa8f7a521d4945d23b27314735
Avery Yang <averyyang2674@gmail.com>
2022-06-29 14:20:26 -0500

    complete function

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,30 +1,78 @@
-$(document).ready(onReady);
+$(onReady);
 
 function onReady() {
+    //GET request
     getSongs();
     $('#add').on('click', postSong);
+    //6. DELETE SONG click listener
+    $('#songsTableBody').on('click', '.btn-delete', deleteSong);
+    //10. PUT SONG click listener
+    //make sure the name is correct
+    //make sure we're calling the function
+    $('#songsTableBody').on('click', '.btn-rank', voteOnSong);
 }
 
 // get artist data from the server
 function getSongs() {
-    $("#songsTableBody").empty();
+    $('#songsTableBody').empty();
     $.ajax({
         type: 'GET',
-        url: '/songs'
+        url: '/songs',
     }).then(function (response) {
-        console.log("GET /songs response", response);
+        console.log('GET /songs response', response);
         // append data to the DOM
         for (let i = 0; i < response.length; i++) {
+            //4. create a DELETE and EDIT button
+            //5. add data-id
+            //9. create 2 buttons for rankup and rankdown
             $('#songsTableBody').append(`
                 <tr>
                     <td>${response[i].artist}</td>
                     <td>${response[i].track}</td>
                     <td>${response[i].rank}</td>
                     <td>${response[i].published}</td>
+                    <td>
+                    <button
+                        data-id=${response[i].id}
+                        data-direction="up"
+                        class="btn-rank">👍</button>
+                    <button
+                        data-id=${response[i].id}
+                        data-direction="down"
+                        class="btn-rank">💩</button>
+                    <button
+                        data-id=${response[i].id}
+                        class="btn-delete"
+                    >Delete</button>
+                    </td>
                 </tr>
             `);
         }
     });
+}
+
+// DELETE function
+function deleteSong() {
+    console.log("We're in DELETE");
+    //0. grab the songId
+    let songId = $(this).data('id');
+    //1. AJAX DELETE request
+    $.ajax({
+        //2. change method to DELETE
+        method: 'DELETE',
+        url: `/songs/${songId}`,
+    })
+        //3. if successfully deleted, then...
+        .then(function (response) {
+            console.log('Boop! Deleted!', response);
+            //4. UPDATE the information on DOM
+            // GET request to get all the songs
+            getSongs();
+        })
+        //4. if DELETE failed, then...
+        .catch(function (error) {
+            alert('Error in DELETE function on client.js:', error);
+        });
 }
 
 function postSong() {
@@ -32,17 +80,40 @@ function postSong() {
         artist: $('#artist').val(),
         track: $('#track').val(),
         rank: $('#rank').val(),
-        published: $('#published').val()
-    }
+        published: $('#published').val(),
+    };
     $.ajax({
         type: 'POST',
         url: '/songs',
-        data: payloadObject
-    }).then( function (response) {
+        data: payloadObject,
+    }).then(function (response) {
         $('#artist').val(''),
-        $('#track').val(''),
-        $('#rank').val(''),
-        $('#published').val('')
+            $('#track').val(''),
+            $('#rank').val(''),
+            $('#published').val('');
         getSongs();
     });
+}
+
+//voteOnSong function
+function voteOnSong() {
+    // 6. declare songId
+    let songId = $(this).data('id');
+    // 7. declare voteDirection
+    let voteDirection = $(this).data('direction');
+    // 8. PUT AJAX
+    $.ajax({
+        method: 'PUT',
+        url: `/songs/rank/${songId}`,
+        // data = req.body
+        data: { direction: voteDirection },
+    })
+        .then(function () {
+            //retrieve songs
+            getSongs();
+        })
+        .catch(function (error) {
+            //alert user error
+            alert('ERROR on VOTE:', error);
+        });
 }
--- server/routes/songs.router.js
+++ server/routes/songs.router.js
@@ -27,16 +27,76 @@ pool.on('error', (error) => {
     console.log('Error with postgres:', error);
 });
 
-//GET = keep the whack, don't add anything
+//GET request
 router.get('/', (req, res) => {
-    //
-    let queryText = 'SELECT * FROM songs;';
+    let queryText = 'SELECT * FROM "songs" ORDER BY "rank";';
     pool.query(queryText)
         .then((result) => {
             res.send(result.rows);
         })
         .catch((err) => {
-            console.log(`Error making query ${queryText}`, err);
+            console.log(`GET error making query ${queryText}`, err);
+            res.sendStatus(500);
+        });
+});
+
+//GET
+//Concept: grab a specific song with a specific id
+// 1. change the route from '/' to '/:id'
+// example: /songs/:id
+router.get('/:id', (req, res) => {
+    //2. create your dynamic route by id -- can only be letters and numbers
+    const idToGet = req.params.id;
+    //3. create your query params
+    let queryText = 'SELECT * FROM songs WHERE id=$1;';
+    //4. add [idToGet] into pool.query() => sends id to database (parse sql query)
+    pool.query(queryText, [idToGet])
+        .then((result) => {
+            res.send(result.rows);
+        })
+        .catch((err) => {
+            console.log(`GET error making query ${queryText}`, err);
+            res.sendStatus(500);
+        });
+});
+
+//DELETE request
+//1. DELETE can use the SAME URL as the GET request
+router.delete('/:id', (req, res) => {
+    //2. define reqId
+    let reqId = req.params.id;
+    console.log('Delete request for id', reqId);
+    //2. define query -- don't need double quotes but it's for safety
+    let queryText = 'DELETE FROM "songs" WHERE id = $1;';
+    //3. pool.query to send (queryText, [reqId])
+    pool.query(queryText, [reqId])
+        //4. if row is SUCESSFULLY deleted, then...
+        .then(() => {
+            console.log('Song deleted');
+            res.sendStatus(200); //DELETED!!!
+        })
+        //5. if row FAILED to delete, then...
+        .catch((error) => {
+            console.log(`Error DELETEing with query ${queryText}`, error);
+            res.sendStatus(500); // 500 is SERVER ERROR
+        });
+});
+
+// Concept: Grabbing artist
+// 1. change the route from '/' to '/artist/:id'
+// example: /songs/artist/Mahmoud
+router.get('/artist/:artist', (req, res) => {
+    //2. create your dynamic route by artist -- can only be letters and numbers
+    const artistToGet = req.params.artist;
+    //3. filter your query params
+    let queryText = 'SELECT * FROM song WHERE artist = $1;';
+    //4. add [idToGet] into pool.query() => send artist to database
+    pool.query(queryText, [artistToGet])
+        .then((result) => {
+            res.send(result.rows);
+        })
+        .catch((err) => {
+            console.log(`GET error making query ${queryText}`, err);
             res.sendStatus(500);
         });
 });
@@ -62,10 +122,6 @@ let songs = [
     },
 ];
 
-router.get('/', (req, res) => {
-    res.send(songs);
-});
-
 //POST: client => server
 router.post('/', (req, res) => {
     // songs.push(req.body);
@@ -76,8 +132,7 @@ router.post('/', (req, res) => {
     // assign query to queryText
     // this is the query we WOULD be putting into the database
     const queryText = `
-            INSERT INTO "songs" ("artist", "track", "published", "rank")
-            VALUES ('${newSong.artist}', '${newSong.track}', '${newSong.published}');
+            INSERT INTO "songs" ("artist", "track", "published", "rank") VALUES ($1, $2, $3, $4);
         `;
     // checking the queryText to make sure it's the newSong we're sending in!
     console.log(`The query we're sending to postgres:`, queryText);
@@ -92,6 +147,38 @@ router.post('/', (req, res) => {
         .catch((err) => {
             console.log(`Error POSTing to db ${queryText}`, err);
             //error status
+            res.sendStatus(500);
+        });
+});
+
+// Change rank on my song - body will say up or down
+router.put('/rank/:id', (req, res) => {
+    let songId = req.params.id;
+    // Direction will come from the request body
+    // Drection expected to be up/down
+    let direction = req.body.direction;
+    // declare queryText
+    let queryText;
+
+    if (direction === 'up') {
+        // use rank-1, so it get's closer to the awesome rank of 1
+        queryText = `UPDATE "songs" SET rank = rank-1 WHERE id=$1`;
+    } else if (direction == 'down') {
+        queryText = `UPDATE "songs" SET rank = rank+1 WHERE id=$1`;
+    } else {
+        // If we don't get an expected direction, send back bad status
+        res.sendStatus(500);
+        return; // Do it now, don't run code below //exit function on server
+    }
+    //send => database
+    pool.query(queryText, [songId])
+        .then((dbResponse) => {
+            //.rows just sends the rows from database
+            res.send(dbResponse.rows);
+        })
+        .catch((err) => {
+            console.log(`Error UPDATEing with query: ${err.message} ${err}`);
+            //error status code
             res.sendStatus(500);
         });
 });
